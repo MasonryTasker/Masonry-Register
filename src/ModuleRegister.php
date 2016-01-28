d5c44db47a85c8dd84fb793e45de252c0cca3a6f
Daniel Mason <daniel.mason@thefoundry.co.uk>
2016-01-28 12:06:05 +0000

    Incorrect parameter order fixed.

--- src/ModuleRegister.php
+++ src/ModuleRegister.php
@@ -150,11 +150,14 @@ class ModuleRegister implements ModuleRegisterInterface
             && is_array($array['workerModules'])
         ) {
             foreach ($array['workerModules'] as $name => $module) {
-                $this->workerModules[$name] = new WorkerModuleDefinition(
-                    (array)$module[WorkerModuleDefinition::KEY_WORKERS],
-                    (array)$module[WorkerModuleDefinition::KEY_DESCRIPTIONS],
-                    (array)$module[WorkerModuleDefinition::KEY_EXTRA]
-                );
+                $name         = $module[WorkerModuleDefinition::KEY_NAME];
+                $workers      = (array)$module[WorkerModuleDefinition::KEY_WORKERS];
+                $descriptions = (array)$module[WorkerModuleDefinition::KEY_DESCRIPTIONS];
+                $extra        = array_key_exists(WorkerModuleDefinition::KEY_EXTRA, $module)
+                    ? (array)$module[WorkerModuleDefinition::KEY_EXTRA]
+                    : [];
+                
+                $this->workerModules[$name] = new WorkerModuleDefinition($name, $workers, $descriptions, $extra);
             }
         }
         return $this;
